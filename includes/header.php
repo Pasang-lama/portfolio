cbbe5a162c573b729dd9003ecfcae318a0089c0a
Pasang-lama <pasanglama66666@gmail.com>
2024-05-21 17:24:46 +0545

    works on the meta tittle

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pasang Lama</title>
+    <title>Pasang Lama a freelancer web designer | Provide web design, SEO, Social media handaling and post design Services</title>
+    <meta name=keywords content="Web Design, Website developement, Social media handaler, technology, SEO, Responsive design, Freelancer, Freelancer web design, Graphics design, Hyolmo, UIUX Desing, Hyolmo People, Freelancer Web Designer, Freelancer UIUX Designer, Hyolmo Creative Link" />
+    <meta name=description content="Are you looking for a web development team to design your website. choose us to turned your vision into reality with precision and efficiency. We provide web design, SEO, Social media handaling and post design services that build brand presence, and drive meaningful interactions to enhance your online community." />
+    <meta property=og:type content=website />
+    <meta property=og:title content="Pasang Lama a freelancer web designer | Provide web design, SEO, Social media handaling and post design Services" />
+    <meta property=og:site_name content="Pasang Lama a freelancer web designer" />
+    <meta property=og:description content="Are you looking for a web development team to design your website. choose us to turned your vision into reality with precision and efficiency. We provide web design, SEO, Social media handaling and post design services that build brand presence, and drive meaningful interactions to enhance your online community." />
+    <meta name="google-site-verification" content="T9kKitT8qTdfu1LRuvawg-0hJj82WBH4TTJoTkCOlN8" />
     <!-- custome css  -->
     <link rel="stylesheet" href="css/style.css">
     <!-- fontwsome css  -->
