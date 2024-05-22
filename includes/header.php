d3ea9e912b94af91c6849d31de3080c43962d17f
Pasang-lama <pasanglama66666@gmail.com>
2024-05-22 20:17:50 +0545

    works on the social media thumbnail

--- includes/header.php
+++ includes/header.php
@@ -7,11 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pasang Lama a freelancer web designer | Provide web design, SEO, Social media handaling and post design Services</title>
     <meta name=keywords content="Web Design, Website developement, Social media handaler, technology, SEO, Responsive design, Freelancer, Freelancer web design, Graphics design, Hyolmo, UIUX Desing, Hyolmo People, Freelancer Web Designer, Freelancer UIUX Designer, Hyolmo Creative Link" />
-    <meta name=description content="Are you looking for a web development team to design your website. choose us to turned your vision into reality with precision and efficiency. We provide web design, SEO, Social media handaling and post design services that build brand presence, and drive meaningful interactions to enhance your online community." />
+    <meta name=description content="Are you looking for a web development team to design your website. choose us to turned your vision into reality with precision and efficiency. We provide web solution, SEO, Social media handaling and post design services that build brand presence." />
     <meta property=og:type content=website />
-    <meta property=og:title content="Pasang Lama a freelancer web designer | Provide web design, SEO, Social media handaling and post design Services" />
+    <meta property="og:image" content="images/Pasan-Sir.jpg"/>
+    <meta property=og:title content="Pasang Lama a freelancer web designer | Provide web solution, SEO, Social media handaling and post design Services" />
     <meta property=og:site_name content="Pasang Lama a freelancer web designer" />
-    <meta property=og:description content="Are you looking for a web development team to design your website. choose us to turned your vision into reality with precision and efficiency. We provide web design, SEO, Social media handaling and post design services that build brand presence, and drive meaningful interactions to enhance your online community." />
+    <meta property=og:description content="Are you looking for a web development team to design your website. choose us to turned your vision into reality with precision and efficiency. We provide web solution, SEO, Social media handaling and post design services that build brand presence." />
     <meta name="google-site-verification" content="T9kKitT8qTdfu1LRuvawg-0hJj82WBH4TTJoTkCOlN8" />
     <!-- custome css  -->
     <link rel="stylesheet" href="css/style.css">
